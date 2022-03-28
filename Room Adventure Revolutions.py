0887904ef5aecfa02caee1b39d96879bf28e3fdb
Worldferret428 <102531213+Worldferret428@users.noreply.github.com>
2022-03-28 09:13:53 -0500

    Update Room Adventure Revolutions.py

--- Room Adventure Revolutions.py
+++ Room Adventure Revolutions.py
@@ -40,6 +40,66 @@ class Room:
     @exits.setter
     def exits(self, value):
         self._exits = value
+    
+    @property
+    def items(self):
+        return self._items
+    
+    @items.setter
+    def items(self, value):
+        self._items = value
+    
+    @property
+    def grabbables(self):
+        return self._grabbables
+    
+    @grabbables.setter
+    def grabbables(self, value):
+        self._grabbables = value
+    
+    #adds an exit to the room
+    #the item is a string
+    #the room is an instance of a room
+    def addExit(self, exit, room):
+        #append the exit and room to the appropriate
+        #dictionary
+        self._exits[exit] = room
+    
+    #adds an item to the room as a string and the desc is a string as well to describe the item
+    def addItem(self, item, desc):
+        #append the item and desc to appropriate dictionary
+        self._items[item] = desc
+    
+    #adds a grabbable as a string
+    def addGrabbable(self, item):
+        #append the item to the list
+        self._grabbables.append(item)
+        
+    #removes a grabbable
+    def delGrabbable(self, item):
+        #removes grabbable from list
+        self._grabbables.remove(item)
+    
+    #returns a string description of the room
+    def __str__(self):
+        #the room name
+        s = "You are in {}.\n".format(self.name)
+        
+        #the visible items
+        s += "You see: "
+        for item in self.items,keys():
+            s += item + " "
+        s += "\n"
+        
+        #the exits from the room
+        s += "Exits: "
+        for exit in self.exits.keys.keys():
+            s += exit + " "
+        return s
+
+#the game class
+#inherits from the Fram class of Tkinter
+
 
     
 ##########################################################
